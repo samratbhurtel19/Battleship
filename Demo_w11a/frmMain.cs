dfc66ecbec1b68f8ca8651b8a447b227ee71892b
samratbhurtel19 <samratjayanta.bhurtel@dcmail.ca>
2024-11-19 13:45:27 -0500

    Commit

--- Demo_w11a/frmMain.cs
+++ Demo_w11a/frmMain.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Demo_w11a.Classes;
+using Demo_w11a_MoreBindingInheritance.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +27,17 @@ namespace Demo_w11a
             this.locationsTableAdapter.Fill(this.employeeSampleDataSetJoined.locations);
             // TODO: This line of code loads data into the 'employeeSampleDataSetJoined.departments' table. You can move, or remove it, as needed.
             this.departmentsTableAdapter.Fill(this.employeeSampleDataSetJoined.departments);
+
+        }
+        public void InheritanceTester()
+        {
+            Car car = new Car();
+            Car car2 = new Car("vin1","Honda","Civic",2024, Color.Black,"Sedan",4);
+            Truck truck = new Truck();
+            Truck truck2 = new Truck("vin2", "Ford", "F150", 2024, "V8", 2, 10000);
+
+            Vehicle vehicle = new Vehicle();
+
 
         }
     }
